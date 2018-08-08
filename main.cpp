afd279bd90e7a1bfa397922d24e542b54b5d22c1
ndropp92 <ndropp92@aol.com>
2018-08-07 22:04:03 -0400

    Update main.cpp
    removed main code

--- main.cpp
+++ main.cpp
@@ -11,13 +11,6 @@ using namespace std;
 int main() {
 
 	Stack<int> intstack;
-	int x = 0;
-	while (x < 40) {
-		intstack.push(x%5);
-		x++;
-
-	} 
-	cout << intstack.pop() << '\n';
-	intstack.print();
+	
 	return 0;
 }
